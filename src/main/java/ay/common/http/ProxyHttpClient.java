f6946bb44d6fc5afed955cc10922b892d2f7a7c3
shizhida <shizhida@kingsoft.com>
2017-05-26 19:27:33 +0800

    thread local

--- src/main/java/ay/common/http/ProxyHttpClient.java
+++ src/main/java/ay/common/http/ProxyHttpClient.java
@@ -6,10 +6,14 @@ import ay.common.http.proxy.ProxyInfo;
 import ay.common.http.proxy.ProxyPool;
 import ay.common.util.CommonConfig;
 import ay.spider.SpiderContext;
+import ay.zhihu.RequestCenter;
+import ay.zhihu.api.QuestionApi;
+import ay.zhihu.pojo.Question;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -30,30 +34,25 @@ public class ProxyHttpClient {
     OkHttpClient httpClient;
     Log log = LogFactory.getLog(ProxyHttpClient.class);
 
+    ThreadLocal<ProxyInfo> currentProxy = null;
+
+
     public ProxyHttpClient (){
         httpClient = new OkHttpClient.Builder()
-                .proxySelector(new ProxySelector() {
-                    @Override
-                    public List<Proxy> select(URI uri) {
-                        ProxyInfo proxyInfo = ProxyPool.get();
-//                        System.out.println(proxyInfo);
-                        Proxy proxy = new Proxy(Proxy.Type.HTTP,proxyInfo.address());
-                        proxyInfo.revert();
-                        return Arrays.asList(proxy);
-                    }
-                    @Override
-                    public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
-                        log.error("exception",ioe);
-                    }
-                }).connectTimeout(CommonConfig.getHttpTimeout(), TimeUnit.MILLISECONDS)
+                .proxySelector(new PoolProxySelector()).connectTimeout(CommonConfig.getHttpTimeout(), TimeUnit.MILLISECONDS)
                 .readTimeout(CommonConfig.getHttpTimeout(), TimeUnit.MILLISECONDS)
                 .writeTimeout(CommonConfig.getHttpTimeout(), TimeUnit.MILLISECONDS).build();
     }
 
-    public String getString(String url){
-        Request request = new Request.Builder().url(url).get().build();
+    public synchronized String getString(String url){
+        Request request = new Request.Builder().url(url)
+                .header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36").get().build();
         try {
-            return httpClient.newCall(request).execute().body().string();
+            String content = httpClient.newCall(request).execute().body().string();
+            if(StringUtils.isNotEmpty(content)){
+                currentProxy.get().revert();
+            }
+            return content;
         } catch (IOException e) {
             return "";
         }
@@ -75,8 +74,27 @@ public class ProxyHttpClient {
         for(int i=0;i<100;i++) {
             String web = httpClient.getString("https://www.zhihu.com/question/39479153");
             System.out.println(web);
+            Question question = QuestionApi.questionInfo(web);
+            System.out.println(question.getTopics());
         }
         context.stop();
+    }
+
+    class PoolProxySelector extends ProxySelector {
+
+        @Override
+        public List<Proxy> select(URI uri) {
+            currentProxy.set( ProxyPool.get());
+//                        System.out.println(proxyInfo);
+            Proxy proxy = new Proxy(Proxy.Type.HTTP,currentProxy.get().address());
+            return Arrays.asList(proxy);
+        }
+
+        @Override
+        public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+            ProxyPool.remove(currentProxy.get());
+            log.error(ioe.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/ay/common/http/proxy/source/XdProxy.java
@@ -0,0 +1,11 @@
+package ay.common.http.proxy.source;
+
+/**
+ * Created by SHIZHIDA on 2017/5/26.
+ */
+public class XdProxy {
+//    http://www.xdaili.cn/freeproxy.html
+//    http://www.66ip.cn/pt.html
+//    http://www.kuaidaili.com/free/outha/
+//    http://www.89ip.cn/
+}
